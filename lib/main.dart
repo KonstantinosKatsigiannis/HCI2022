02d39551d7f122b830e47aedb21f0b689f790e63
KonstantinosKatsigiannis <el19127@mail.ntua.gr>
2023-02-21 01:19:51 +0200

    main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,157 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/gestures.dart';
-import 'dart:ui';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:myapp/utils.dart';
-// import 'package:myapp/prototype/iphone-14-2.dart';
-// import 'package:myapp/prototype/iphone-14-3.dart';
-// import 'package:myapp/prototype/iphone-14-4.dart';
-// import 'package:myapp/prototype/user-images-user-images.dart';
-// import 'package:myapp/prototype/iphone-14-7.dart';
-// import 'package:myapp/prototype/iphone-14-13.dart';
-// import 'package:myapp/prototype/iphone-14-14.dart';
-// import 'package:myapp/prototype/iphone-14-15.dart';
-// import 'package:myapp/prototype/iphone-14-17.dart';
-// import 'package:myapp/prototype/iphone-14-18.dart';
-// import 'package:myapp/prototype/iphone-14-20.dart';
-// import 'package:myapp/prototype/iphone-14-21.dart';
-// import 'package:myapp/prototype/frame-2608488.dart';
-// import 'package:myapp/prototype/iphone-14-22.dart';
-// import 'package:myapp/prototype/iphone-14-23.dart';
-// import 'package:myapp/prototype/iphone-14-24.dart';
-// import 'package:myapp/prototype/iphone-14-25.dart';
-// import 'package:myapp/prototype/iphone-14-26.dart';
-// import 'package:myapp/prototype/iphone-14-27.dart';
-// import 'package:myapp/prototype/keyboard.dart';
-// import 'package:myapp/prototype/iphone-14-28.dart';
-// import 'package:myapp/prototype/iphone-14-29.dart';
-// import 'package:myapp/prototype/input-date-picker.dart';
-// import 'package:myapp/prototype/text-field.dart';
-// import 'package:myapp/prototype/iphone-14-30.dart';
-// import 'package:myapp/prototype/iphone-14-31.dart';
-// import 'package:myapp/prototype/iphone-14-32.dart';
-// import 'package:myapp/prototype/drop-down-list.dart';
-// import 'package:myapp/prototype/iphone-14-33.dart';
-// import 'package:myapp/assets/state-layer.dart';
-// import 'package:myapp/assets/iphone-14-2.dart';
-// import 'package:myapp/assets/iphone-14-3.dart';
-// import 'package:myapp/assets/iphone-14-4.dart';
-// import 'package:myapp/assets/user-images-user-images.dart';
-// import 'package:myapp/assets/iphone-14-7.dart';
-// import 'package:myapp/assets/iphone-14-13.dart';
-// import 'package:myapp/assets/iphone-14-14.dart';
-// import 'package:myapp/assets/iphone-14-15.dart';
-// import 'package:myapp/assets/iphone-14-17.dart';
-// import 'package:myapp/assets/docked-input-date-picker-desktop.dart';
-// import 'package:myapp/assets/iphone-14-18.dart';
-// import 'package:myapp/assets/iphone-14-20.dart';
-// import 'package:myapp/assets/iphone-14-21.dart';
-// import 'package:myapp/assets/frame-2608488.dart';
-// import 'package:myapp/assets/iphone-14-22.dart';
-// import 'package:myapp/assets/iphone-14-23.dart';
-// import 'package:myapp/assets/iphone-14-24.dart';
-// import 'package:myapp/assets/iphone-14-25.dart';
-// import 'package:myapp/assets/iphone-14-26.dart';
-// import 'package:myapp/assets/iphone-14-27.dart';
-// import 'package:myapp/assets/keyboard.dart';
-// import 'package:myapp/assets/iphone-14-28.dart';
-// import 'package:myapp/assets/iphone-14-29.dart';
-// import 'package:myapp/assets/input-date-picker.dart';
-// import 'package:myapp/assets/text-field.dart';
-// import 'package:myapp/assets/iphone-14-30.dart';
-// import 'package:myapp/assets/iphone-14-31.dart';
-// import 'package:myapp/assets/iphone-14-32.dart';
-// import 'package:myapp/assets/iphone-14-33.dart';
-
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-	@override
-	Widget build(BuildContext context) {
-	return MaterialApp(
-		title: 'Flutter',
-		debugShowCheckedModeBanner: false,
-		scrollBehavior: MyCustomScrollBehavior(),
-		theme: ThemeData(
-		primarySwatch: Colors.blue,
-		),
-		home: Scaffold(
-		body: SingleChildScrollView(
-			child: Scene(),
-		),
-		),
-	);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Calendar',
+      home: CalendarScreen(),
+    );
+  }
+}
+
+class CalendarScreen extends StatefulWidget {
+  @override
+  _CalendarScreenState createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  DateTime _selectedDate = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Calendar'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SfCalendar(
+              initialDisplayDate: DateTime.now(),
+              view: CalendarView.month,
+              //dataSource: _getCalendarDataSource(),
+              onTap: (details) {
+                if (details.appointments == null || details.appointments!.isEmpty) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetailsScreen(selectedDate: details.date!),
+                    ),
+                  );
+                } else {
+                  print(details.appointments!.first);
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _AppointmentDataSource {
+  _AppointmentDataSource(List<Appointment> appointments);
+}
+
+class DetailsScreen extends StatefulWidget {
+  final DateTime selectedDate;
+
+  const DetailsScreen({required this.selectedDate});
+
+  @override
+  _DetailsScreenState createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _descriptionController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _titleController = TextEditingController();
+    _descriptionController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _descriptionController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add birthday'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Date: ${widget.selectedDate.day}/${widget.selectedDate.month}/${widget.selectedDate.year}',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(
+                hintText: 'Name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              controller: _descriptionController,
+              maxLines: 1,
+              decoration: InputDecoration(
+                hintText: 'Phone number',
+                border: OutlineInputBorder(),
+              ),
+            ),            
+            SizedBox(height: 20.0),
+            TextField(
+              controller: _descriptionController,
+              maxLines: 4,
+              decoration: InputDecoration(
+                hintText: 'Wishlist',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              controller: _descriptionController,
+              maxLines: 4,
+              decoration: InputDecoration(
+                hintText: 'Other information',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                final newAppointment = Appointment(
+                  startTime: widget.selectedDate,
+                  endTime: widget.selectedDate.add(Duration(hours: 1)),
+                  subject: _titleController.text,
+                  notes: _descriptionController.text,
+                  color: Colors.blue,
+                );
+                Navigator.pop(context, newAppointment);
+              },
+              child: Text('Add birthday'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
