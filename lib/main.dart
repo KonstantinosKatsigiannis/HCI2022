f45b86ee3390bd198dd6d54f9d07eaaca929710f
ntua-el19127 <116357312+ntua-el19127@users.noreply.github.com>
2023-02-24 00:11:44 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:myapp/AddPage.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart' show Appointment, CalendarView, SfCalendar;
+import 'tofilter.dart';
+import '/AddPage.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'My Calendar',
       home: CalendarScreen(),
     );
@@ -15,7 +19,10 @@ class MyApp extends StatelessWidget {
 }
 
 class CalendarScreen extends StatefulWidget {
+  const CalendarScreen({super.key});
+
   @override
+  // ignore: library_private_types_in_public_api
   _CalendarScreenState createState() => _CalendarScreenState();
 }
 
@@ -26,14 +33,25 @@ class _CalendarScreenState extends State<CalendarScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My Calendar'),
+        leading: IconButton(
+        tooltip:'Options',
+        icon: const Icon(Icons.menu), 
+        onPressed: () { 
+        Navigator.push(context,MaterialPageRoute(builder: (BuildContext context) { return ToFilter(); },
+        ),
+        );
+         },),
+        title: const Text('My Calendar'),
+        backgroundColor: Color.fromARGB(255, 154, 192, 236),
       ),
+      
       body: Column(
         children: [
           Expanded(
             child: SfCalendar(
               initialDisplayDate: DateTime.now(),
               view: CalendarView.month,
+              backgroundColor: Color.fromARGB(255, 232, 211, 217),
               //dataSource: _getCalendarDataSource(),
               onTap: (details) {
                 if (details.appointments == null || details.appointments!.isEmpty) {
@@ -44,7 +62,11 @@ class _CalendarScreenState extends State<CalendarScreen> {
                     ),
                   );
                 } else {
-                  print(details.appointments!.first);
+                  if (kDebugMode) {
+                    if (kDebugMode) {
+                      print(details.appointments!.first);
+                    }
+                  }
                 }
               },
             ),
@@ -53,66 +75,90 @@ class _CalendarScreenState extends State<CalendarScreen> {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
       floatingActionButton: FloatingActionButton(
-        onPressed:() {
+            
+              child: const Icon(Icons.add),
+              onPressed:(){
         Navigator.push(context,MaterialPageRoute(builder: (BuildContext context){
           return  AddScreen(selectedDate: DateTime.now());
         },),
       );  
-        }, 
-        backgroundColor: Colors.purple,
-        child: const Icon(Icons.add),
-           ),
+        },
+              tooltip: 'Add birthday',
+              backgroundColor: Colors.purple,
+
+            ),
+            
     );
   }
+
+  
 }
 
+// ignore: unused_element
 class _AppointmentDataSource {
- _AppointmentDataSource(List<Appointment> appointments);
+  _AppointmentDataSource(List<Appointment> appointments);
 }
 
 class DetailsScreen extends StatefulWidget {
   final DateTime selectedDate;
 
-  const DetailsScreen({required this.selectedDate});
+  const DetailsScreen({super.key, required this.selectedDate});
 
   @override
+  // ignore: library_private_types_in_public_api
   _DetailsScreenState createState() => _DetailsScreenState();
 }
 
 class _DetailsScreenState extends State<DetailsScreen> {
-  TextEditingController _titleController = TextEditingController();
-  TextEditingController _descriptionController = TextEditingController();
-
-  @override
-  void initState() {
-   super.initState();
-    _titleController = TextEditingController();
-    _descriptionController = TextEditingController();
-  }
-
-  @override
-  void dispose() {
-    _titleController.dispose();
-    _descriptionController.dispose();
-    super.dispose();
-  }
+ TextEditingController _titleController = TextEditingController();
+  TextEditingController _titleController1 = TextEditingController();
+  TextEditingController _titleController2 = TextEditingController();
+  TextEditingController _titleController3 = TextEditingController();
+  TextEditingController _titleController4 = TextEditingController();
+  TextEditingController _titleController5 = TextEditingController();
+  
+  
+  // ignore: prefer_typing_uninitialized_variables
+  var floatingActionButton;
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 232, 211, 217),
       appBar: AppBar(
-        title: Text('Add birthday'),
+        backgroundColor: Color.fromARGB(255, 154, 192, 236),
       ),
       body: Padding(
-       padding: const EdgeInsets.all(20.0),
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-           Text(
+            
+            Text(
               'Date: ${widget.selectedDate.day}/${widget.selectedDate.month}/${widget.selectedDate.year}',
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              style: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 20.0),
+        
+                        
+           SizedBox(
+             width: 80.0,
+             height: 80.0, 
+             child: IconButton(
+              icon: Icon(Icons.account_box), 
+              iconSize: 80,
+              onPressed:(){},
+              tooltip: 'Add Photo',
+              color:  Color.fromARGB(255, 34, 3, 50),
+             ),
+      ),
+            // ignore: prefer_const_constructors
+            SizedBox(height: 10.0,
+            child: const DecoratedBox(
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 244, 54, 190)),
+            ),
+            ),
             TextField(
               controller: _titleController,
               decoration: InputDecoration(
@@ -120,56 +166,57 @@ class _DetailsScreenState extends State<DetailsScreen> {
                 border: OutlineInputBorder(),
               ),
             ),
-            SizedBox(height: 20.0),
+            SizedBox(height: 10.0),
             TextField(
-              controller: _titleController,
+              controller: _titleController2,
               maxLines: 1,
               decoration: InputDecoration(
                 hintText: 'Phone number',
                 border: OutlineInputBorder(),
               ),
             ),            
-            SizedBox(height: 20.0),
+            SizedBox(height: 10.0),
             TextField(
-              controller: _descriptionController,
+              controller: _titleController2,
+              maxLines: 1,
+              decoration: InputDecoration(
+                hintText: 'Category',
+                border: OutlineInputBorder(),
+                
+              ),
+            ),
+            SizedBox(height: 10.0),
+            TextField(
+              controller: _titleController3,
+              maxLines: 1,
+              decoration: InputDecoration(
+                hintText: 'Date',
+                border: OutlineInputBorder(),
+              ),
+            ),          
+            SizedBox(height: 10.0),
+            TextField(
+              controller: _titleController4,
               maxLines: 4,
               decoration: InputDecoration(
                 hintText: 'Wishlist',
                 border: OutlineInputBorder(),
               ),
-            ),
-            SizedBox(height: 20.0),
+            ),          
+            SizedBox(height: 10.0),
             TextField(
-              controller: _descriptionController,
+              controller: _titleController5,
               maxLines: 4,
               decoration: InputDecoration(
-                hintText: 'Other information',
+                hintText: 'Other Information',
                 border: OutlineInputBorder(),
               ),
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                final newAppointment = Appointment(
-                  startTime: widget.selectedDate,
-                  endTime: widget.selectedDate.add(Duration(hours: 1)),
-                  subject: _titleController.text,
-                 notes: _descriptionController.text,
-                  color: Colors.blue,
-                );
-                Navigator.pop(context, newAppointment);
-              },
-              child: Text('Add birthday'),
-            ),
+            ),          
           ],
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-     floatingActionButton: FloatingActionButton(
-        onPressed:() {}, 
-        backgroundColor: Colors.purple,
-        child: const Icon(Icons.add),
-           ),
+   
     );
   }
 }
+
