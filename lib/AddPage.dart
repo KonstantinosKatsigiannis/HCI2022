4a6185192e37cfed51988fdc8133b552baea54a1
ntua-el19127 <116357312+ntua-el19127@users.noreply.github.com>
2023-02-24 00:13:58 +0200

    Update AddPage.dart

--- lib/AddPage.dart
+++ lib/AddPage.dart
@@ -12,39 +12,49 @@ class AddScreen extends StatefulWidget {
   State<AddScreen> createState() => _AddScreenState();
 }
 
-
 class _AddScreenState extends State<AddScreen> {
+TextEditingController _titleController = TextEditingController();
+  TextEditingController _titleController1 = TextEditingController();
+  TextEditingController _titleController2 = TextEditingController();
+  TextEditingController _titleController3 = TextEditingController();
+  TextEditingController _titleController4 = TextEditingController();
+  TextEditingController _titleController5 = TextEditingController();
 
-
-  TextEditingController _titleController = TextEditingController();
-  TextEditingController _descriptionController = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-    _titleController = TextEditingController();
-    _descriptionController = TextEditingController();
-  }
-
-  @override
-  void dispose() {
-    _titleController.dispose();
-    _descriptionController.dispose();
-    super.dispose();
-  }
+  var floatingActionButton;
 
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-        appBar: AppBar(
-        title: Text('Add birthday'),
+     backgroundColor: Color.fromARGB(255, 232, 211, 217),
+      appBar: AppBar(
+        title: Text('New Entry'),
+        backgroundColor: Color.fromARGB(255, 154, 192, 236),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(10.0),
+        padding: const EdgeInsets.all(20.0),
         child: Column(
+        
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 10.0),
+                       
+           SizedBox(
+             width: 80.0,
+             height: 80.0, 
+             child: IconButton(
+              icon: Icon(Icons.account_box), 
+              iconSize: 80,
+              onPressed:(){},
+              tooltip: 'Add Photo',
+              color:  Color.fromARGB(255, 34, 3, 50),
+             ),
+      ),
+            // ignore: prefer_const_constructors
+            SizedBox(height: 10.0,
+            child: const DecoratedBox(
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 244, 54, 190)),
+            ),
+            ),
             TextField(
               controller: _titleController,
               decoration: InputDecoration(
@@ -52,73 +62,72 @@ class _AddScreenState extends State<AddScreen> {
                 border: OutlineInputBorder(),
               ),
             ),
-            SizedBox(height: 10.0),
+            SizedBox(height: 10.0,
+            child: const DecoratedBox(
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 244, 54, 190)),
+            ),
+            ),
             TextField(
-              controller: _titleController,
-              maxLines: 1,
+              controller: _titleController1,
               decoration: InputDecoration(
                 hintText: 'Phone Number',
                 border: OutlineInputBorder(),
               ),
-            ),            
+            ),
             SizedBox(height: 10.0),
             TextField(
-              controller: _titleController,
+              controller: _titleController2,
+              maxLines: 1,
               decoration: InputDecoration(
                 hintText: 'Category',
                 border: OutlineInputBorder(),
               ),
+            ),    
+            SizedBox(height: 10.0,
+            child: const DecoratedBox(
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 244, 54, 190)),
             ),
-            SizedBox(height: 10.0),
+            ),
             TextField(
-              controller: _titleController,
+              controller: _titleController3,
               decoration: InputDecoration(
                 hintText: 'Date',
                 border: OutlineInputBorder(),
               ),
-            ),
+            ),        
             SizedBox(height: 10.0),
             TextField(
-              controller: _descriptionController,
+              controller: _titleController4,
               maxLines: 4,
               decoration: InputDecoration(
                 hintText: 'Wishlist',
                 border: OutlineInputBorder(),
+                
               ),
             ),
             SizedBox(height: 10.0),
             TextField(
-              controller: _descriptionController,
+              controller: _titleController5,
               maxLines: 4,
               decoration: InputDecoration(
                 hintText: 'Other information',
                 border: OutlineInputBorder(),
               ),
             ),
+            // ignore: prefer_const_constructors
+            
+            //Add Birthday Button
             SizedBox(height: 10.0),
             ElevatedButton(
-              onPressed: () {
-                final newAppointment = Appointment(
-                  startTime: widget.selectedDate,
-                  endTime: widget.selectedDate.add(Duration(hours: 1)),
-                  subject: _titleController.text,
-                  notes: _descriptionController.text,
-                  color: Colors.blue,
-                );
-                Navigator.pop(context, newAppointment);
-              },
-              child: Text('Add birthday'),
+              onPressed: () {},
+              child:Text('Add birthday'),
             ),
           ],
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-      floatingActionButton: FloatingActionButton(
-        onPressed:() {}, 
-        backgroundColor: Colors.purple,
-        child: const Icon(Icons.add),
-           ),
-
-    );
+ 
+    );   
   }
 }
